852143bd5b01ee2e13ad4a1ddbfb4671b653bfdb
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2025-03-20 08:29:38 +0100

    Add a minimal example, useful for prototyping
    This is something I found useful to get a first impression when working out a new criterion.
    It may at some point also serve as a minimal, fully working and self-contained example of how to use Stacie.

---
+++ tools/quick.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Minimalistic quick example of using stacie."""
+
+import matplotlib as mpl
+import numpy as np
+
+from stacie import ChebyshevModel, compute_spectrum, estimate_acint, plot_results, summarize_results
+
+# Generate input sequences with a simple Markov process.
+nseq = 100
+sequences = [np.zeros(nseq)]
+nstep = 8192
+rng = np.random.default_rng(4)
+for _ in range(nstep):
+    step = sequences[-1] * 0.9
+    step += rng.normal(0, 0.1, nseq)
+    sequences.append(step)
+sequences = np.array(sequences).T
+
+# Estimate the autocorrelation integral, print and plot the results.
+spectrum = compute_spectrum(sequences)
+result = estimate_acint(spectrum, ChebyshevModel(2), verbose=True)
+print()
+print(summarize_results(result))
+mpl.rc_file("../docs/source/examples/matplotlibrc")
+plot_results("quick.pdf", result)
