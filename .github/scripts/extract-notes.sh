874883b898b01609e737c6cc91f98e9dae1ec316
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2025-12-24 20:15:02 +0100

    Fix extract-notes.sh script

--- .github/scripts/extract-notes.sh
+++ .github/scripts/extract-notes.sh
@@ -9,19 +9,18 @@ GITREF=${2}
 if [[ "${GITREF}" == refs/tags/* ]]; then
     TAG="${GITREF#refs/tags/}"
     VERSION="${TAG#v}"
-    MACRO_MESO=$(echo "${VERSION}" | cut -d. -f1,2)
 else
     TAG="unreleased"
     VERSION="Unreleased"
-    MACRO_MESO="dev"
 fi
+DASHTAG="${TAG//./-}"
 
 # Extract the release notes from the changelog
 sed -n "/## \[${VERSION}\]/, /## /{ /##/!p }" docs/source/development/changelog.md > notes.md
 
 # Add a link to the release notes
-URL="https://${OWNER}.github.io/${SLUG}/${MACRO_MESO}/changelog/#${TAG}"
-echo "See [docs/changelog/#${TAG}](${URL}) for more details." >> notes.md
+URL="https://${OWNER}.github.io/${SLUG}/development/changelog.html#${DASHTAG}"
+echo "See [changelog#${TAG}](${URL}) for more details." >> notes.md
 
 # Remove leading and trailing empty lines
 sed -e :a -e '/./,$!d;/^\n*$/{$d;N;};/\n$/ba' -i notes.md
